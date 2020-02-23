b8e5276d2880e4e382986d74f60479bb112b5d05
ZhengMaster2020 <2105195274@qq.com>
2020-02-23 22:00:03 +0800

    新增搜索页面，专栏页面，书籍简介详情页面

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,15 +1,16 @@
+
 var bookTitle = ['人类群星闪耀时', '追风筝的人']
 
 var outline = '《人类群星闪耀时》内容介绍：拜占廷的沦陷、亨徳尔的一生、一个流星般的天才、好不完美。描述了农村青年的奋斗生活以及感情经历。改革开放初期的不完美事物，完全爆率出社会百态生活'
 
 var outline1 = '《追风筝的人》12岁的阿富汗富家少爷阿米尔与仆人哈桑情同手足。然而，在一场风筝比赛后，发生了一件悲惨不堪的事，阿米尔为自己的懦弱感到自责和痛苦，逼走了哈桑，不久，自己也跟随父亲逃往美国。'
 
-Page({
+var appInstance = getApp()
+console.log(appInstance.globalData, 'appInstance')
 
-  /**
-   * 页面的初始数据
-   */
+Page({
   data: {
+    searchValue: '',
     bookOutline: [outline, outline1],
     bookNodes: [{
       name: 'a',
@@ -22,9 +23,6 @@ Page({
         text: bookTitle[0]
       }],
     }],
-    tap() {
-      console.log('tap')
-    },
     swiperImg: ["img-1", "img-2", "img-3", '4', '5'],
     background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
     indicatorDots: true,
@@ -32,6 +30,30 @@ Page({
     autoplay: true,
     interval: 2000,
     duration: 500
+  },
+  // 点击跳转专栏内容
+  toTopicBlock () {
+    wx.navigateTo({ url: '../topic/topic'})
+  },
+  toSearchPage () {
+    wx.navigateTo({ url: '../search/search' })
+  },
+  // 搜索书籍
+  searchBook (e) {
+    // if (!this.data.searchValue) return wx.showToast({
+    //   title: '输入值不能为空',
+    //   icon: 'none',
+    //   mask: true,
+    //   duration: 3000
+    // })
+    // console.log(this.data.searchValue, 'e')
+    wx.navigateTo({ url: '../search/search' })
+  },
+  // 查看书籍详情内容
+  toDetial(e) {
+    wx.navigateTo({
+      url: '../details/detail',
+    })
   },
   changeIndicatorDots() {
     this.setData({
